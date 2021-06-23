7c0ffbe088a9e625841eb34ba6379803a3886ded
Matan Navon <matann@tamuzgroup.co.il>
2021-06-23 21:55:50 +0300

    fixing aspect ratio

--- src/screens/WebcamExercise.js
+++ src/screens/WebcamExercise.js
@@ -9,7 +9,6 @@ import data from '../data/data.json';
 // eslint-disable-next-line no-undef
 const { innerWidth: width, innerHeight: height } = window;
 
-const aspectRatio = height < width ? height / width : width / height;
 const videoConstraints = {
   aspectRatio: width / height,
 };
